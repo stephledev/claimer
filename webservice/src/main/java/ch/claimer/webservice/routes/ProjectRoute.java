dc877596dc89c9686c3208c9ca02362d5311535f
momox1389 <momcilo.bekcic@hotmail.com>
2015-04-27 09:16:14 +0200

    Project-Route
    Project-Route

--- webservice/src/main/java/ch/claimer/webservice/routes/ProjectRoute.java
+++ webservice/src/main/java/ch/claimer/webservice/routes/ProjectRoute.java
@@ -1,58 +1,122 @@
-//package ch.claimer.webservice.routes;
-//
-//import javax.annotation.security.RolesAllowed;
-//import javax.ws.rs.FormParam;
-//import javax.ws.rs.GET;
-//import javax.ws.rs.PUT;
-//import javax.ws.rs.Path;
-//import javax.ws.rs.PathParam;
-//import javax.ws.rs.core.Response;
-//
-//import ch.claimer.webservice.controller.ProjectController;
-//import ch.claimer.webservice.services.RouteResponseHandlerService;
-//
-//
-///**
-// * Defines RESTful routes for project specific interactions. 
-// * Maps the controller according to the URL pattern
-// * 
-// * @author Momcilo Bekcic 
-// */
-//@Path("/")
-//public class ProjectRoute {
-//	
-//	private ProjectController<Response> controller;
-//
-//	public ProjectRoute() {
-//		this.controller = new ProjectController<Response>(new RouteResponseHandlerService());
-//	}
-//	
-//	/**
-//	 * Maps the controller to show project(s) of a supervisor
-//	 * 
-//	 * @param id supervisor identifier of project(s) to show supplied by the URL
-//	 * 
-//	 * @return Response from the controller
-//	 */
-//	@GET
-//	@RolesAllowed({"EDITOR","INTERN"})
-//	@Path("/project/supervisor/{id}")
-//	public Response showProjectsBySupervisor(@PathParam("id") int id) {
-//		return controller.showBySupervisor(id);
-//	}
-//	
-//	/**
-//	 * Maps the controller to update the specified project instance
-//	 * 
-//	 * @param data data of the project instance to update supplied by a form
-//	 * 		  
-//	 * @return Response from the controller
-//	 */
-//	@PUT
-//	@RolesAllowed({"EDITOR","INTERN"})
-//	@Path("project")
-//	public Response updateProject(@FormParam("data") String data) {
-//		return controller.update(data);
-//	} 
-//	
-//}
+package ch.claimer.webservice.routes;
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+import ch.claimer.shared.models.Comment;
+import ch.claimer.shared.models.Project;
+import ch.claimer.webservice.controller.Controller;
+
+
+/**
+ * Definiert die REST-Routes der Projekte. Zeigt den Controller gemäss der URL-Pattern.
+ * Diese Klasse wird gemäss dem Dokument "Rollen und Rechte" erstellt
+ * 
+ * @author Momcilo Bekcic
+ */
+@Path("/")
+public class ProjectRoute {
+
+	private Controller<Project> controller;
+
+	public ProjectRoute() {
+		this.controller = new Controller<Project>(Project.class);
+	}
+
+	/**
+	 * Benutzt den Controller um die Bauleiter zu lesen
+	 * 
+	 * @param id-Bauleiter-Identifizierer um diese gemäss der URL anzuzeigen
+	 * 
+	 * @return Antwort vom "controller"
+	 */
+	@GET
+	@Path("/project/supervisor/{id}")
+	public Response showBySupervisor(@PathParam("id") int id) {
+		return controller.showByProperty("person_id", id);
+	}
+
+	/**
+	 * Benutzt den "Controller" um die Kategorie
+	 * 
+	 * @param id-Kategorie-Identifizierer um diese gemäss der URL anzuzeigen
+	 * 
+	 * @return Antwort vom Controller
+	 */
+	@GET
+	@Path("/project/category/{id}")
+	public Response showByCategory(@PathParam("id") int id) {
+		return controller.showByProperty("person_id", id);
+	}
+	
+	/**
+	 * Benutzt den Controller um den Status anzuzeigen
+	 * 
+	 * @param id-Status-Identifizierer um es gemäss der URL anzuzeigen
+	 * 
+	 * @return Antwort vom Controller
+	 */
+	@GET
+	@Path("/project/state/{id}")
+	public Response showByState(@PathParam("id") int id) {
+		return controller.showByProperty("person_id", id);
+	}
+	
+	
+	/**
+	 * Benutzt den Controller um den Typ
+	 * 
+	 * @param id-Typ-Identifizierer um dieses gemäss der URL anzuzeigen
+	 * 
+	 * @return Antwort vom Controller
+	 */
+	@GET
+	@Path("/project/type/{id}")
+	public Response showByType(@PathParam("id") int id) {
+		return controller.showByProperty("person_id", id);
+	}
+	
+	
+	/**
+	 * Benutzt den Controller nur um den Bauleiter anzuzeigen
+	 * 
+	 * @param id-Bauleiter-Identifizierer um dieses gemäss der URL anzuzeigen
+	 * 
+	 * @return Antwort vom Controller
+	 */
+	@GET
+	@Path("/project/supervisor/{id}")
+	public Response showSupervisor(@PathParam("id") int id) {
+		return controller.showByProperty("person_id", id);
+	}
+	
+	/**
+	 * Benutzt den Controller nur um die Ansprechperson
+	 * 
+	 * @param id-Ansprechperson-Identifizierer um dieses gemäss der URL anzuzeigen
+	 * 
+	 * @return Antwort vom Controller
+	 */
+	@GET
+	@Path("/project/contact/{id}")
+	public Response showContact(@PathParam("id") int id) {
+		return controller.showByProperty("person_id", id);
+	}
+	
+	/**
+	 * Benutzt den Controller nur um den Logeintrag anzuzeigen
+	 * 
+	 * @param id-Logeintrag-Identifizierer um dieses gemäss der URL anzuzeigen
+	 * 
+	 * @return Antwort vom Controller
+	 */
+	@GET
+	@Path("/project/logEntry/{id}")
+	public Response showlogEntry(@PathParam("id") int id) {
+		return controller.showByProperty("person_id", id);
+	}
+	
+}
