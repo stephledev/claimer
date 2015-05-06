aa0bc99be7ec59d8066b4113930049bea3d84d3c
RaoulA <raoul.ackermann@gmail.com>
2015-05-06 17:18:53 +0200

    RMI Test (unfinished)

--- appserver/test/main/java/ch/claimer/appserver/rmi/TestRMI.java
+++ appserver/test/main/java/ch/claimer/appserver/rmi/TestRMI.java
@@ -1,7 +1,133 @@
 package ch.claimer.appserver.rmi;
 
-public class TestRMI {
+import static org.junit.Assert.assertEquals;
 
+import java.rmi.Naming;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+
+import ch.claimer.appserver.repositories.Repository;
+import ch.claimer.appserver.repositories.eclipselink.EclipseLinkRepository;
+import ch.claimer.shared.methods.Method;
+import ch.claimer.shared.models.GCEmployee;
+import ch.claimer.shared.models.Issue;
+import ch.claimer.shared.models.Project;
+import ch.claimer.shared.models.SCEmployee;
+import ch.claimer.shared.models.State;
+import ch.claimer.shared.models.Supervisor;
+
+public class TestRMI {
 	
+	private static Repository<Project> projectRepo;
+	private static Repository<GCEmployee> gcEmployeeRepo;
+	private static Repository<Issue> issueRepo;
+	private static Repository<SCEmployee> scEmployeeRepo;
+	private static Repository<Supervisor> supervisorRepo;
+	private static Method<State> stateMethod;
+
+	@SuppressWarnings("unchecked")
+	@BeforeClass
+	public static void oneTimeSetUp() {
+		Config config = ConfigFactory.load();
+		stateMethod = (Method<State>) Naming.lookup(config.getString("rmi.url") + "Status");
+
+		
+		State s1 = new State();
+		s1.setName("Status1");
+		stateMethod.create(s1);
+		
+		GCEmployee g1 = new GCEmployee();
+		g1.setLastname("Nachname1");
+		gcEmployeeRepo.create(g1);
+		
+		GCEmployee g2 = new GCEmployee();
+		g2.setLastname("Nachname2");
+		g2.setFirstname("Firstname2");
+		gcEmployeeRepo.create(g2);
+		
+		SCEmployee sce1 = new SCEmployee();
+		sce1.setLastname("sceNachname1");
+		sce1.setFirstname("sceVorname1");
+		scEmployeeRepo.create(sce1);
+		
+		Supervisor sup1 = new Supervisor();
+		sup1.setLastname("SupName1");
+		supervisorRepo.create(sup1);
+		
+		
+		Project p1 = new Project();
+		p1.setName("Projectname1");
+		p1.setPlace("Luzern");
+		p1.setStreet("Zentralstrasse 9");
+		projectRepo.create(p1);
+		
+		Project p2 = new Project();
+		p2.setName("Projectname2");
+		p2.setStreet("Street2");
+		p2.setSupervisor(sup1);
+		projectRepo.create(p2);
+		
+		Issue iss1 = new Issue();
+		iss1.setDescription("Issue1");
+		iss1.setProject(p1);
+		issueRepo.create(iss1);
+		
+	}
+	
+	@Test
+    public void testGetById() {
+    	assertEquals("Status1", stateMethod.getById(1).getName());
+    	assertEquals("Nachname1", gcEmployeeRepo.getById(1).getLastname());
+    	assertEquals("Projectname1", projectRepo.getById(1).getName());
+    }
+    
+    @Test
+    public void testGetAll() {
+    	assertEquals(2, gcEmployeeRepo.getAll().size());
+    	assertEquals("Projectname2", projectRepo.getAll().get(1).getName());
+      	
+    }
+    
+    @Test
+    public void testGetByProperty() {
+    	assertEquals("Projectname1", projectRepo.getByProperty("place", "Luzern").get(0).getName());
+    	assertEquals("Nachname2", gcEmployeeRepo.getByProperty("firstname", "Firstname2").get(0).getLastname());
+
+    }
+  
+    @Test
+    public void testGetByRelation(){
+    	assertEquals("Issue1", issueRepo.getByRelation(Project.class, 1).get(0).getDescription());
+    	assertEquals("Projectname2", projectRepo.getByRelation(Supervisor.class, 4).get(0).getName());
+
+    }
+    
+    @Test
+    public void testCreate(){
+     Project p3 = new Project();
+     p3.setName("Projectname3");
+     projectRepo.create(p3);
+     assertEquals("Projectname3", projectRepo.getById(3).getName());
+     
+    }
+    
+    @Test
+    public void testUpdate(){
+     Project p1 = projectRepo.getById(1);
+     p1.setName("ProjectX");
+     projectRepo.update(p1);
+     assertEquals("ProjectX", projectRepo.getById(1).getName());
+    }
+
+    @Test
+    public void testDelete(){
+    	stateMethod.delete(1);
+    	assertEquals(null, stateMethod.getById(1));
+    }
+
 	
 }
