6aed1210dda78d7a435ed7b35c09df0a19f7988c
stephowski <beelerstephan@gmail.com>
2015-05-06 22:25:32 +0200

    no need anymore

--- webservice/src/main/java/ch/claimer/webservice/services/ConverterService.java
+++
@@ -1,9 +0,0 @@
-package ch.claimer.webservice.services;
-
-import java.util.List;
-
-public interface ConverterService<T> {
-	public T read(String data, Class<T> clazz);
-	public String write(T t);
-	public String write(List<T> list);
-}
