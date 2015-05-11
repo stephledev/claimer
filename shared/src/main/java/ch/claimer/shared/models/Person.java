885ddb736ff858b0a6c144a8fe8a93ddf7c111bd
stephowski <beelerstephan@gmail.com>
2015-05-11 14:33:35 +0200

    Person json mapping

--- shared/src/main/java/ch/claimer/shared/models/Person.java
+++ shared/src/main/java/ch/claimer/shared/models/Person.java
@@ -11,6 +11,8 @@ import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.OneToOne;
 
+import org.codehaus.jackson.annotate.JsonTypeInfo;
+
 /**
  * Representiert die Entität Person und gewährleistet Zugriff auf die Eigenschaften 
  * der Klasse. Beispielsweise benutzt die SCEmployee- und GCEmployee-Klasse die Eigenschaften.
@@ -23,6 +25,7 @@ import javax.persistence.OneToOne;
 
 @Entity
 @Inheritance(strategy=InheritanceType.JOINED)
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS)
 public abstract class Person implements Serializable, Model {
 
 	private static final long serialVersionUID = -7793558619197649513L;
