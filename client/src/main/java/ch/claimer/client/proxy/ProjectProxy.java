ed89f9b24ddd206f3634a2270598cae200ef70e3
RaoulA <raoul.ackermann@gmail.com>
2015-05-15 10:52:07 +0200

    Project Proxy update

--- client/src/main/java/ch/claimer/client/proxy/ProjectProxy.java
+++ client/src/main/java/ch/claimer/client/proxy/ProjectProxy.java
@@ -5,6 +5,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -29,7 +30,7 @@ public interface ProjectProxy {
 	@GET
 	@Path("project/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
-	String getById();
+	String getById(@PathParam("id")int id);
 	
 	@GET
 	@Path("project/supervisor/{id}")
